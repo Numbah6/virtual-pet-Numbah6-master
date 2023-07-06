8aed4e0e6d9c6ad2719830a7bc65e774ccae4e7a
Numbah6 <89164785+Numbah6@users.noreply.github.com>
2023-07-05 22:30:13 -0400

    added questions and switch case to do something with those answers

--- virtual-pet-Numbah6-master/src/main/java/virtual_pet/VirtualPetApplication.java
+++ virtual-pet-Numbah6-master/src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,32 +1,137 @@
 package virtual_pet;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-import java.time.Clock;
-import java.time.Duration;
-import java.time.Instant;
+
+import javax.sound.sampled.SourceDataLine;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class VirtualPetApplication {
+    private static String name;
+    private static int hunger = 100;
+    private static int thirst = 100;
+    private int bladder = 0;
+    private static int boredLevel = 0;
+    private int sickness = 0;
+    private static int tiredness = 0;
+    private int eatPoints = 50;
+    
+    
 
+
+
+    public static void eat(int petChoice){
+    hunger += 30;
+    thirst -= 5;
+    System.out.println(name+"has eaten a hearty meal!");
+}
+
+public static void drink(int petChoice){
+    thirst += 30;
+    System.out.println(name+" has drunk some water!");
+}
+
+public static void play(int petChoice){
+    boredLevel -= 20;
+    System.out.println("You took "+name+" to the park to play with them");
+}
+
+public static void sleep(int petChoice){
+    tiredness -= 20;
+    System.out.println(name+" Went to sleep...now"+name+" they feels refreshed");
+}
+
+public static void lazy(int petChoice){
+    System.out.println("You and "+name+" decided to be lazy right now");
+    hunger -= 10;
+    thirst -= 10;
+    boredLevel += 10;
+    tiredness += 10;
+}
+
+public static void tick(){
+    hunger -= 5;
+    thirst -= 5;
+    boredLevel += 5;
+    tiredness += 5;
+}
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
+        List<String> questions = new ArrayList<>();
+        questions.add("What would you like to do with "+name+" ?");
+        questions.add("1. Feed?");
+        questions.add("2. Give water?");
+        questions.add("3. Play with?");
+        questions.add("4. Tell to go to sleep? ");
+        questions.add("5. Do nothing. ");
+        
+        //Adding scanner to take user input
         Scanner userInput = new Scanner(System.in);
-        //Implementing a way to display date and time for the game 
-        LocalDateTime passageOfGameTime = LocalDateTime.now();
-        DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("dd-MMM-yyy HH:mm:ss");
-        String formattedPassage = passageOfGameTime.format(formatTime); 
-        System.out.println(formattedPassage);
+        boolean playing = true;
+        //Looping through the questions
+        // for (int i = 0; i < questions.size(); i++){
+        //     String question = questions.get(i);
+        //     System.out.println(question);
+        //     int petChoice = userInput.nextInt();
 
-        //Importing a clock for the game
-        Clock gameClock = Clock.systemUTC();
-        Instant time = gameClock.instant();
-        Clock inGameClock = Clock.tick(gameClock, Duration.ofHours(1));
+        while(playing){
+            for (String question : questions){
+            System.out.println(question);
+            playing = false;
+        }
+        
+            int petChoice = userInput.nextInt();
+            userInput.nextLine();
+            switch(petChoice){
+                case 1:
+                eat(petChoice);
+                break;
+
+                case 2:
+                drink(petChoice);;
+
+                case 3:
+                play(petChoice);
+                break;
+
+                case 4:
+                sleep(petChoice);
+                break;
+
+                case 5:
+                lazy(petChoice);
+                break;
+
+                default:
+                System.out.println("Thats not a valid answer, please try again");
+                break;
+            }
+            
+            // A way to break the loop
+            System.out.println("Would you like to keep playing? Yes/No");
+            String keepPlaying = userInput.nextLine();
+            if(keepPlaying.equalsIgnoreCase("yes")){
+                playing = true;
+            }
+            else {
+                System.out.println("Goodbye "+name+" will miss you!");
+            userInput.close(); 
+            }
+            
+        }
+
+        
         
 
-
-
+        
+        
     
-    }
+        //Closing the scanner
+        
 }
+}
+
+
+
 
     
 
--- virtual-pet-Numbah6-master/src/test/java/virtual_pet/VirtualPetTest.java
+++ virtual-pet-Numbah6-master/src/test/java/virtual_pet/VirtualPetTest.java
@@ -15,6 +15,7 @@ public class VirtualPetTest {
     private ArrayList<Integer> number_options = new ArrayList<>();
     private ArrayList<String> pet_options = new ArrayList<>();
     private ArrayList<String> special_modifiers = new ArrayList();
+    private ArrayList<String> questions = new ArrayList();
 
     public int petChoice = userInput.nextInt();
 
@@ -36,77 +37,100 @@ public VirtualPetTest (String name){
 
 }
 
-public ArrayList<String> listOptions(){
-    System.out.println("What would like to do?");
-    pet_options.add("Feed "+name);
-    pet_options.add("Give water to "+name);
-    pet_options.add("Play with "+name);
-    pet_options.add("Tell "+name+"to go to sleep");
-    pet_options.add("Do nothing");
- 
-}
+// public void tick(){
+//     questions.add("What would like to do?");
+//     questions.add("1.Feed "+name);
+//     questions.add("2.Give water to "+name);
+//     questions.add("3.Play with "+name);
+//     questions.add("4.Tell "+name+"to go to sleep");
+//     questions.add("5.Do nothing.");
+    
+//     for (String question : questions){
+//             System.out.println(question);
+//             int petChoice = userInput.nextInt();
+//         }
 
-public void tick(){
-    while (petChoice >=5) {
+// }
+
+public void choices(){
+    while (petChoice >= 5 ) {
         switch(petChoice){
             case 1:
-            System.out.println("What would you like to do?");
+            eat();
             break;
-
             case 2:
-            System.out.println("Feed "+name+" ?");
-            
+            drink();
+            break;
+            case 3:
+            play();
+            break;
+            case 4:
+            sleep();
+            break;
+            case 5:
+
+        
         }
     }
 }
 
 
+// private String[] listQuestions() {
+//     return null;
+// }
+
 //creating methods for updating the instance variables 
-public void eat (int eatPoints){
-    this.hunger = this.hunger + eatPoints;
+public void eat(){
+    this.hunger = this.hunger + 30;
     System.out.println(name+"has eaten a hearty meal!");
 }
 
-public void drink(int waterPoints){
-    this.thirst = this.thirst + waterPoints;
+public void drink(){
+    this.thirst = this.thirst + 30;
     System.out.println(name+" has drunk some water!");
 }
 
-public void play(int playPoints){
-    this.boredLevel = this.boredLevel - playPoints;
+public void play(){
+    this.boredLevel = this.boredLevel - 20;
     System.out.println("You took "+name+" to the park to play with them");
 }
 
-public void sleep(int sleepPoints) {
-    this.tiredness = this.tiredness - sleepPoints;
+public void sleep(){
+    this.tiredness = this.tiredness - 20;
     System.out.println(name+" Went to sleep...now"+name+" they feels refreshed");
 }
 
 
 //creating methods that update instance variables to correspond with the methods
-public void hungry(int eatPoints){
-    this.hunger = this.hunger - eatPoints;
+public void hungry(){
+    this.hunger = this.hunger - 25;
     System.out.println(name+" is really hungry...give it something to eat!");
 }
 
-public void thirsty(int waterPoints){
-    this.thirst = this.thirst - waterPoints;
+public void thirsty(){
+    this.thirst = this.thirst - 25;
     System.out.println(name+" is is thirsty...give it something to drink!");
 }
 
-public void bored(int playPoints) {
-    this.boredLevel = this.boredLevel + playPoints;
+public void bored() {
+    this.boredLevel = this.boredLevel + 15;
     System.out.println(name+" is getting bored just staring out the window...take it out to the park to have some fun! ");
 }
 
-public void sleepy(int sleepPoints){
-    this.tiredness = this.tiredness + sleepPoints;
+public void sleepy(){
+    this.tiredness = this.tiredness + 15;
     System.out.println(name+" is getting tired");
 }
 
 
 //random and logical occurring methods and instance variables to make the pet feel more alive 
-
+public void lazy(){
+    System.out.println("You and "+name+" decided to be lazy right now");
+    this.hunger = this.hunger - 10;
+    this.thirst = this.thirst - 10;
+    this.boredLevel = this.boredLevel + 10;
+    this.tiredness = this.tiredness + 10;
+}
 }
 
 
